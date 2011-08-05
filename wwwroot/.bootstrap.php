62ab9d4cb2568a97d1877e3dba819e78741aa609
Toby Miller <tobius.miller@gmail.com>
2011-08-05 11:26:06 -0400

    this collection of changes pulls the default app out of the ellipsis directory and allows for multiple applications as /.appname, auto loaded if passed to run('appname')

---
+++ wwwroot/.bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Ellipsis Bootstrap
+ *
+ * "Kick the tires and light the fires, big daddy."
+ *
+ * @author Toby Miller <tobius.miller@gmail.com>
+ * @license MIT <http://www.opensource.org/licenses/mit-license.php>
+ * @package ellipsis
+ */
+
+require_once './.ellipsis/ellipsis.php';
+
+// run application(s)
+Ellipsis::run('default');
+
+?>
