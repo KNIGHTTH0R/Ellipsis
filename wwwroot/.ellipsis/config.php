cda203e89c015afb9d5205079c8d610e5f55c3c2
Toby Miller <tobius.miller@gmail.com>
2011-10-26 17:10:33 -0400

    updated docs and added a maintenance route for expiring cached files

--- wwwroot/.ellipsis/config.php
+++ wwwroot/.ellipsis/config.php
@@ -12,12 +12,12 @@
  */
 
 
-/**
- * default route/cache instructions
- */
+// block access to config file
 Ellipsis::route('^\/config\.php$', function(){
     Ellipsis::fail(404);
 }, 31536000);
+
+// block access to lib directory
 Ellipsis::route('^\/lib\/.*$', function(){ 
     Ellipsis::fail(404); 
 }, 31536000);
@@ -28,6 +28,12 @@ Ellipsis::route('^\/info\.php$', function(){
     phpinfo(); 
     return false;
 }, 3600);
+
+// expire cached files
+Ellipsis::route('^\/maintenance\/expire$', function(){
+    Ellipsis::expire();
+    return false;
+});
 
 // cache the favorite icon
 //Ellipsis::route('^\/favicon\.ico$/', '/favicon.ico', 31536000);
