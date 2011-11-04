76b1d7d32efd4c3d8c4b336e2e2ce9e6ba2f2fd4
Toby Miller <tobius.miller@gmail.com>
2011-11-03 23:08:43 -0400

    added doc block

--- wwwroot/.default/404.php
+++ wwwroot/.default/404.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * default 404 error handler
+ *
+ * @package default
+ */
+
 $message = (isset($_ENV['PARAMS']['message']) ? $_ENV['PARAMS']['message'] : "Missing Page");
 
 echo "404: $message";
