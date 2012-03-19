f2880e4d98aff57c50cde4b473abc81c4839096b
David Cox-Espenlaub <dcoxespenlaub@resource.com>
2012-03-19 16:59:15 -0400

    Fixed undefined current app for website routes, 404s for destination files

--- base/apps/ellipsis/ellipsis.php
+++ base/apps/ellipsis/ellipsis.php
@@ -190,6 +190,7 @@ class Ellipsis {
                 // process rewrite_path
                 if (!preg_match('/\$\{/', $route['rewrite_path'])){
                     $_ENV['CACHE_TIME'] = $route['cache'];
+
                     self::load_path($route['rewrite_path']);
                 }
             }
@@ -735,7 +736,15 @@ class Ellipsis {
         header("Content-Type: $mime_type");
 
         // load path resource
-        $htdocs_root = "{$_ENV['APPS'][$_ENV['CURRENT']]['APP_SRC_ROOT']}/htdocs";
+        if(is_null($_ENV['CURRENT']))
+        {
+            // website route
+            $htdocs_root = "{$_ENV['WEBSITE_ROOT']}/htdocs/";
+            
+        }else{
+            // app route
+            $htdocs_root = "{$_ENV['APPS'][$_ENV['CURRENT']]['APP_SRC_ROOT']}/htdocs/";
+        }
         if (is_file("{$htdocs_root}{$path}")){
             if (preg_match('/\.php$/', $path)){
                 include "{$htdocs_root}{$path}";
